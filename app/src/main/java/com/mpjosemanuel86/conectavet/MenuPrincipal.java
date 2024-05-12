3aa41672e1ef2ae43af37c743b03fb2cad89d4a1
Jose <mpjosemanuel86@gmail.com>
2024-05-12 14:35:25 +0200

    Registro firebase implementado

--- app/src/main/java/com/mpjosemanuel86/conectavet/MenuPrincipal.java
+++ app/src/main/java/com/mpjosemanuel86/conectavet/MenuPrincipal.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,6 +19,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -22,11 +31,23 @@ public class MenuPrincipal extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
 
+    TextView NombresPrincipal, CorreoPrincipal;
+    ProgressBar progressBarDatos;
+
+    DatabaseReference Usuarios;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Agenda Online");
+
+        NombresPrincipal = findViewById(R.id.NombresPrincipal);
+        CorreoPrincipal= findViewById(R.id.CorreoPrincipal);
+        progressBarDatos = findViewById(R.id.progressBarDatos);
+
+        Usuarios = FirebaseDatabase.getInstance().getReference("Usuarios");
         CerrarSesion = findViewById(R.id.CerrarSesion);
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
@@ -34,10 +55,57 @@ public class MenuPrincipal extends AppCompatActivity {
         CerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              SalirAplicacion();
+                SalirAplicacion();
             }
         });
 
+    }
+    @Override
+    protected void  onStart(){
+        ComprobarInicioSesion();
+        super.onStart();
+    }
+
+    private void ComprobarInicioSesion(){
+        if (user!=null){
+            //El usuario ha iniciado sesion
+            CargaDeDatos();
+
+        }else {
+            //Lo dirigirá al MainActivity
+            startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+            finish();
+        }
+    }
+    private void CargaDeDatos(){
+        Usuarios.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Si el usuario existe
+                if (snapshot.exists()){
+                    //El progressbar se oculta
+                    progressBarDatos.setVisibility(View.GONE);
+                    //Los TexView se muestran
+                    NombresPrincipal.setVisibility(View.VISIBLE);
+                    CorreoPrincipal.setVisibility(View.VISIBLE);
+
+                    //Obtener los datos
+                    String nombres = "" +snapshot.child("nombres").getValue();
+                    String correo = "" +snapshot.child("correo").getValue();
+
+                    //Setear los datos en los respectivos TextView
+                    NombresPrincipal.setText(nombres);
+                    CorreoPrincipal.setText(correo);
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void SalirAplicacion() {
