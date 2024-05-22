8cb812375df4161a88980ca4f237cdf8b4fda39a
Jose <mpjosemanuel86@gmail.com>
2024-05-22 13:22:26 +0200

    Reyclerview clientes funcionando

--- app/src/main/java/com/mpjosemanuel86/conectavet/adapter/ClienteAdapter.java
+++ app/src/main/java/com/mpjosemanuel86/conectavet/adapter/ClienteAdapter.java
@@ -2,6 +2,8 @@ package com.mpjosemanuel86.conectavet.adapter;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
@@ -22,79 +25,87 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.mpjosemanuel86.conectavet.R;
 import com.mpjosemanuel86.conectavet.model.Cliente;
+import com.mpjosemanuel86.conectavet.ui.fragment.CrearClienteFragment;
 
 import java.util.List;
 public class ClienteAdapter extends FirestoreRecyclerAdapter<Cliente, ClienteAdapter.ViewHolder> {
     private FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
     Activity activity;
+    FragmentManager fm;
 
 
-
-    public ClienteAdapter(@NonNull FirestoreRecyclerOptions<Cliente> options, Activity activity) {
+    public ClienteAdapter(@NonNull FirestoreRecyclerOptions<Cliente> options, Activity activity, FragmentManager fm) {
         super(options);
         this.activity = activity;
+        this.fm = fm;
     }
+
     @Override
     protected void onBindViewHolder(@NonNull ViewHolder viewHolder, int i, @NonNull Cliente cliente) {
         DocumentSnapshot documentSnapshot = getSnapshots().getSnapshot(viewHolder.getAdapterPosition());
         final String id = documentSnapshot.getId();
+
         viewHolder.direccionCliente.setText(cliente.getDireccionCliente());
         viewHolder.nombreCliente.setText(cliente.getNombreCliente());
         viewHolder.telefonoCliente.setText(cliente.getTelefonoCliente());
+
+        viewHolder.btn_editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(activity, CrearClienteFragment.class);
+                i.putExtra("id_cliente",id);
+              //  activity.startActivity(i);
+
+                CrearClienteFragment crearClienteFragment = new CrearClienteFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("id_cliente", id);
+                crearClienteFragment.setArguments(bundle);
+                crearClienteFragment.show(fm, "abrir fragment");
+            }
+        });
         viewHolder.btn_eliminar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 borrarCliente(id);
-                }
+            }
         });
     }
-    private void borrarCliente(String id) {
-        String idString = String.valueOf(id);
-        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-        builder.setMessage("¿Estás seguro de eliminar este cliente de forma permanente?")
-                        .setTitle("Confirmar Eliminación")
-                                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
 
-                                        mFirestore.collection("cliente").document(idString).delete()
-                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                    @Override
-                                                    public void onSuccess(Void unused) {
-                                                        Toast.makeText(activity, "Cliente eliminado correctamente", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                })
-                                                .addOnFailureListener(new OnFailureListener() {
-                                                    @Override
-                                                    public void onFailure(@NonNull Exception e) {
-                                                        Toast.makeText(activity, "Error al eliminar cliente", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                });
-                                    }
-                                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener(){
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.dismiss();
-                    }
-                    });
-        AlertDialog dialog= builder.create();
-                dialog.show();
-                }
+    private void borrarCliente(String id) {
+        mFirestore.collection("cliente").document(id).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(activity, "Cliente eliminado correctamente", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(activity, "Error al eliminar cliente", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_cliente, parent, false);
         return new ViewHolder(v);
     }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView direccionCliente, nombreCliente, telefonoCliente;
-        ImageButton btn_eliminar;
+        ImageButton btn_eliminar, btn_editar;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
             direccionCliente = itemView.findViewById(R.id.tvDireccion);
             nombreCliente = itemView.findViewById(R.id.tvNombre);
             telefonoCliente = itemView.findViewById(R.id.tvTelefono);
             btn_eliminar = itemView.findViewById(R.id.btn_eliminar);
+            btn_editar = itemView.findViewById(R.id.btn_editar);
         }
     }
 }
--- app/src/main/java/com/mpjosemanuel86/conectavet/ui/fragment/CrearClienteFragment.java
+++ app/src/main/java/com/mpjosemanuel86/conectavet/ui/fragment/CrearClienteFragment.java
@@ -1,5 +1,6 @@
 package com.mpjosemanuel86.conectavet.ui.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -15,6 +16,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.mpjosemanuel86.conectavet.R;
 import java.util.HashMap;
@@ -22,13 +24,19 @@ import java.util.Map;
 
 public class CrearClienteFragment extends DialogFragment {
 
+    String id_cliente;
     Button btnGuardarDatos;
     EditText nombreCliente, direccionCliente, telefonoCliente;
     private FirebaseFirestore mfirestore;
+    private FirebaseAuth mAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        if(getArguments()!=null){
+            id_cliente = getArguments().getString("id_cliente");
+        }
     }
 
     @Override
@@ -37,30 +45,87 @@ public class CrearClienteFragment extends DialogFragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_crear_cliente, container, false);
         mfirestore = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         nombreCliente = v.findViewById(R.id.editTextNombreCliente);
         direccionCliente = v.findViewById(R.id.editTextDireccionCliente);
         telefonoCliente = v.findViewById(R.id.editTextTelefonoCliente);
-
         btnGuardarDatos = v.findViewById(R.id.btnGuardarDatos2);
-        btnGuardarDatos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nombreClientePet = nombreCliente.getText().toString().trim();
-                String direccionClientePet = direccionCliente.getText().toString().trim();
-                String telefonoClientePet = telefonoCliente.getText().toString().trim();
 
-                if (nombreClientePet.isEmpty() || direccionClientePet.isEmpty() || telefonoClientePet.isEmpty()) {
-                    Toast.makeText(getContext(), "Ingresar todos los datos", Toast.LENGTH_SHORT).show();
-                } else {
-                    postCliente(nombreClientePet, direccionClientePet, telefonoClientePet);
+        if(id_cliente == null||id_cliente==""){
+            btnGuardarDatos.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String nombreClientePet = nombreCliente.getText().toString().trim();
+                    String direccionClientePet = direccionCliente.getText().toString().trim();
+                    String telefonoClientePet = telefonoCliente.getText().toString().trim();
+
+                    if (nombreClientePet.isEmpty() || direccionClientePet.isEmpty() || telefonoClientePet.isEmpty()) {
+                        Toast.makeText(getContext(), "Ingresar todos los datos", Toast.LENGTH_SHORT).show();
+                    } else {
+                        postCliente(nombreClientePet, direccionClientePet, telefonoClientePet);
+                    }
                 }
-            }
-        });
+            });
+
+        }else{
+            getcliente();
+            btnGuardarDatos.setText("update");
+
+            btnGuardarDatos.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String nombreClientePet = nombreCliente.getText().toString().trim();
+                    String direccionClientePet = direccionCliente.getText().toString().trim();
+                    String telefonoClientePet = telefonoCliente.getText().toString().trim();
+
+                    if (nombreClientePet.isEmpty() || direccionClientePet.isEmpty() || telefonoClientePet.isEmpty()) {
+                        Toast.makeText(getContext(), "Ingresar todos los datos", Toast.LENGTH_SHORT).show();
+                    } else {
+                        updateCliente(nombreClientePet, direccionClientePet, telefonoClientePet);
+                    }
+                }
+            });
+        }
+
 
         return v;
     }
 
+
+
+
+    private void updateCliente(String nombreClientePet, String direccionClientePet, String telefonoClientePet) {
+
+            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+            if (currentUser != null) {
+                String uid = currentUser.getUid(); // UID del veterinario
+
+                Map<String, Object> clienteData = new HashMap<>();
+                clienteData.put("nombreCliente", nombreClientePet);
+                clienteData.put("direccionCliente", direccionClientePet);
+                clienteData.put("telefonoCliente", telefonoClientePet);
+                clienteData.put("uid", uid); // Mantener el UID del veterinario
+
+                // Crear cliente
+
+                mfirestore.collection("cliente").document(id_cliente).update(clienteData).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(getContext(), "Cliente actualizado exitosamente", Toast.LENGTH_SHORT).show();
+                                getDialog().dismiss();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getContext(), "Error al actualizar datos", Toast.LENGTH_SHORT).show();
+                                Log.e("ERROR", "Error al ingresar datos: " + e.getMessage());
+                            }
+                        });
+            } else {
+                Toast.makeText(getContext(), "Usuario no autenticado", Toast.LENGTH_SHORT).show();
+            }
+        }
     private void postCliente(String nombreClientePet, String direccionClientePet, String telefonoClientePet) {
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if (currentUser != null) {
@@ -94,4 +159,27 @@ public class CrearClienteFragment extends DialogFragment {
             Toast.makeText(getContext(), "Usuario no autenticado", Toast.LENGTH_SHORT).show();
         }
     }
-}
+
+    private void getcliente() {
+        mfirestore.collection("cliente").document(id_cliente).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String nombreClienteText = documentSnapshot.getString("nombreCliente");
+                String direccionClienteText = documentSnapshot.getString("direccionCliente");
+                String telefonoClienteText = documentSnapshot.getString("telefonoCliente");
+
+                nombreCliente.setText(nombreClienteText);
+                direccionCliente.setText(direccionClienteText);
+                telefonoCliente.setText(telefonoClienteText);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Error al obtener los datos", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    }
+
+
+
